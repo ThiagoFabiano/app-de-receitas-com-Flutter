296167c85691afcd265eeb8b7c5e201b558c665c
Thiago Fabiano <thiagoffabiano@gmail.com>
2023-07-19 11:57:49 -0300

    feat: adiciona navigationBottomBar

--- lib/screens/meals.dart
+++ lib/screens/meals.dart
@@ -6,9 +6,9 @@ import 'package:meals/widgets/meal.item.dart';
 import '../models/meal.dart';
 
 class MealsScreen extends StatelessWidget {
-  const MealsScreen({super.key, required this.title, required this.meals});
+  const MealsScreen({super.key, this.title, required this.meals});
 
-  final String title;
+  final String? title;
   final List<Meal> meals;
 
   void SelectMeal(BuildContext context, Meal meal) {
@@ -62,9 +62,12 @@ class MealsScreen extends StatelessWidget {
       );
     }
 
+    if(title == null){
+      return content;
+    }
     return Scaffold(
       appBar: AppBar(
-        title: Text(title),
+        title: Text(title!),
       ),
       body: content,
     );
